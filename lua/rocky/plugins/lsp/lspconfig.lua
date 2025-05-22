1fcb10bab0e546bda80a676fc1ff686561d4738b
Zilver <rockynotchleaf@gmail.com>
2025-05-22 09:15:11 -0500

    fix some lsp stuff

--- lua/rocky/plugins/lsp/lspconfig.lua
+++ lua/rocky/plugins/lsp/lspconfig.lua
@@ -17,6 +17,7 @@ return {
 		local opts = { noremap = true, silent = true }
 		local on_attach = function(client, bufnr)
 			opts.buffer = bufnr
+			vim.lsp.inlay_hint.enable(true)
 
 			-- set keybinds
 			opts.desc = "Show LSP references"
@@ -62,13 +63,23 @@ return {
 		-- used to enable autocompletion (assign to every lsp server config)
 		local capabilities = cmp_nvim_lsp.default_capabilities()
 
-		-- Change the Diagnostic symbols in the sign column (gutter)
-		-- (not in youtube nvim video)
-		local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
-		for type, icon in pairs(signs) do
-			local hl = "DiagnosticSign" .. type
-			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-		end
+		vim.diagnostic.config({
+        signs = {
+                text = {
+                        [vim.diagnostic.severity.ERROR] = " ",
+                        [vim.diagnostic.severity.WARN] = " ",
+                        [vim.diagnostic.severity.INFO] = "󰋼 ",
+                        [vim.diagnostic.severity.HINT] = "󰌵 ",
+                },
+                numhl = {
+                        [vim.diagnostic.severity.ERROR] = "",
+                        [vim.diagnostic.severity.WARN] = "",
+                        [vim.diagnostic.severity.HINT] = "",
+                        [vim.diagnostic.severity.INFO] = "",
+                },
+        },
+		})
+
 		-- configure go server
 		lspconfig["gopls"].setup({
 			capabilities = capabilities,
